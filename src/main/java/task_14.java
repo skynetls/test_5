38e49b270c29adbafb1672c6ea0ffbd7148deb9b
skynetls <lyudocheksi@gmail.com>
2018-11-18 02:31:59 +0300

    correction []

--- src/main/java/task_14.java
+++ src/main/java/task_14.java
@@ -2,7 +2,7 @@
 
 public class task_14 {
     public static void main(String[] args) {
-        int s [] = {2,5,1,8,4,3,6};
+        int [] s = {2,5,1,8,4,3,6};
         for (int i = s.length - 1; i>0; i--){
             for (int j=0; j<i; j++){
                 if (s[j] > s [j+1]) {
